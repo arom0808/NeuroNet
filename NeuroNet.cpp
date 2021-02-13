574f0bd1666a1a246f1263e5af2345efcf13a7af
Roman <arom0808@mail.ru>
2021-02-13 13:25:44 +0300

    first commit

---
+++ NeuroNet.cpp
@@ -0,0 +1,6 @@
+#include "NeuroNet.h"
+
+NeuroNet::NeuroNet()
+{
+
+}
---
+++ NeuroNet.h
@@ -0,0 +1,11 @@
+#ifndef NEURONET_H
+#define NEURONET_H
+
+
+class NeuroNet
+{
+public:
+  NeuroNet();
+};
+
+#endif // NEURONET_H
